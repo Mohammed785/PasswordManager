9ebb176711b456c45740d9ea56058a967b888ec0
Mohammed <mohammednady128@gmail.com>
2022-01-27 13:48:23 +0200

    create user and note models

--- dist/@types.ts
+++ dist/@types.ts
@@ -1,6 +1,18 @@
-export interface Password{
+export interface User {
+    username:string,
+    password:string
+}
+
+export interface Password {
     platform:string
     username:string
     password:string
 }
+
+export interface Note {
+    title:string
+    note:string
+}
+
 export interface PasswordDraft extends Partial<Password> {}
+
--- dist/db.ts
+++ dist/db.ts
@@ -1,42 +1,77 @@
-import { connect, Trilogy, Model, LooseObject } from "trilogy";
+import { connect, Trilogy, Model, LooseObject, SchemaRaw } from "trilogy";
 import { join } from "path";
 import { Password, PasswordDraft } from "./@types";
 
-export const connectDB = () => {
-    return connect(join(__dirname,"..","database.db"), { client: "sql.js" });
+//Schemas
+const UserSchema: SchemaRaw = {
+    username: { type: String, nullable: false, unique: true },
+    password: { type: String, nullable: false },
+    id: "increments",
 };
 
-const PasswordSchema = {
+const PasswordSchema: SchemaRaw = {
     platform: { type: String, nullable: false },
     username: { type: String, nullable: false },
     password: { type: String, nullable: false },
     id: "increments",
 };
 
-export const createModel =(db: Trilogy) => {
+const NoteSchema: SchemaRaw = {
+    title: { type: String, unique: true },
+    note: { type: String, nullable: false },
+    id: "increments",
+};
+
+//DB
+export const connectDB = () => {
+    return connect(join(__dirname, "..", "database.db"), { client: "sql.js" });
+};
+
+export const createModel = (db: Trilogy) => {
     const passwordModel = db.model("Password", PasswordSchema, {
         index: "platform",
         unique: ["platform", "username"],
+        timestamps: true,
     });
-    return passwordModel;
+    const userModel = db.model("User", UserSchema, {
+        index: "username",
+        timestamps: true,
+    });
+    const noteModel = db.model("Note", NoteSchema, { timestamps: true });
+    return { passwordModel, userModel, noteModel };
 };
-
-export const getPassword = (db: Model<LooseObject>,pass: PasswordDraft) => {
+// Password
+export const getPassword = (db: Model<LooseObject>, pass: PasswordDraft) => {
     const passwords = db.find(pass);
     return passwords;
 };
 
-export const createPassword = (db: Model<LooseObject>,pass: Password) => {
-    const password = db.create(pass)
+export const createPassword = (db: Model<LooseObject>, pass: Password) => {
+    const password = db.create(pass);
     return password;
 };
 
-export const updatePassword = (db:Model<LooseObject>,oldPass:PasswordDraft,newPass:PasswordDraft)=>{
+export const updatePassword = (db: Model<LooseObject>,oldPass: PasswordDraft,newPass: PasswordDraft) => {
     const updated = db.update(oldPass, newPass);
     return updated;
-}
+};
 
-export const deletePassword = (db: Model<LooseObject>,pass: PasswordDraft) => {
-    const deleted = db.remove(pass); 
+export const deletePassword = (db: Model<LooseObject>, pass: PasswordDraft) => {
+    const deleted = db.remove(pass);
     return deleted;
 };
+
+// User
+export const findUser = (db: Model<LooseObject>, username:string)=>{
+    const user = db.findOne({username});
+    return user
+}
+
+//Note
+const findAllNotes = (db: Model<LooseObject>, username:string)=>{};
+
+const findNote = (db: Model<LooseObject>, username:string)=>{};
+
+const updateNote = (db: Model<LooseObject>, username:string)=>{};
+
+const deleteNote = (db: Model<LooseObject>, username:string)=>{};
