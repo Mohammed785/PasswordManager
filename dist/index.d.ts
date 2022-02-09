572f85e50dfee909a9a7527bb99808174d3704d1
Mohammed <mohammednady128@gmail.com>
2022-02-09 15:34:32 +0200

    add new global interfaces

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,19 +1,25 @@
-interface Window {
-    api: {
-        getAll: () => void;
-        getPasswords: (criteria) => void;
-        createPassword: (password) => void;
-        updatePassword: (criteria, newPass) => void;
-        deletePassword: (criteria) => void;
-        deletePlatform:(criteria)=>void;
-    };
-}
-
 declare global{
     namespace NodeJS{
         interface ProcessEnv{
             MASTER_SALT:string
         }
+    }
+    interface Window {
+        api: {
+            getAll: () => void;
+            getPasswords: (criteria) => void;
+            createPassword: (password) => void;
+            updatePassword: (criteria, newPass) => void;
+            deletePassword: (criteria) => void;
+            deletePlatform: (criteria) => void;
+        };
+        auth: {
+            login:(username:string,password:string)=>void,
+            register:(username:string,password:string)=>void,
+            needAccount:()=>void,
+            haveAccount:()=>void,
+            forgetPass:()=>void,
+        };
     }
 }
 
