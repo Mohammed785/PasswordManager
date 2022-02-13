5213aff0e75a7b34473025a5afc17c23069ca2d6
Mohammed <mohammednady128@gmail.com>
2022-02-13 20:47:36 +0200

    add error area to main & theme to auth files

---
+++ static/js/ui.js
@@ -0,0 +1,27 @@
+//UI
+const errorArea = document.querySelector(".errors");
+const colors = document.querySelectorAll(".color");
+const toggleBtn = document.getElementsByTagName("svg")[0];
+
+toggleBtn.addEventListener("click", (e) => {
+    document.body.classList.toggle("light-mode");
+});
+colors.forEach((color) => {
+    color.addEventListener("click", (e) => {
+        const theme = color.getAttribute("data-color");
+        console.log(theme);
+        document.body.setAttribute("data-theme", theme);
+    });
+});
+
+
+const clearMsg = setInterval(() => {
+    let first = true;
+    if(errorArea.children.length!=0){
+        const firstChild = errorArea.children[0];
+        if (firstChild) errorArea.removeChild(firstChild);
+    }
+    if(!first && errorArea.children.length===0){
+        clearInterval(clearMsg)
+    }
+},2000);
